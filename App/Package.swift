12661e03fa63e3e323ae8072915f0bbc11159844
janusz1233123 <duzestopy67@proton.me>
2026-06-15 14:58:16 +0200

    Update Package.swift

--- App/Package.swift
+++ App/Package.swift
@@ -1,8 +1,28 @@
-
 import PackageDescription
 
 let package = Package(
-  name: "app", 
-  products: [], 
-  targets: [] 
+    name: "Mobywatel",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .iOSApplication(
+            name: "Mobywatel",
+            targets: ["App"],
+            bundleIdentifier: "pl.gov.mobywatel.test",
+            teamIdentifier: "",
+            displayVersion: "1.0",
+            bundleVersion: "1",
+            appIcon: nil,
+            accentColor: nil,
+            supportedDeviceFamilies: [.iphone, .ipad],
+            supportedInterfaceOrientations: [.portrait]
+        )
+    ],
+    targets: [
+        .executableTarget(
+            name: "App",
+            sources: ["Sources/App"]
+        )
+    ]
 )
